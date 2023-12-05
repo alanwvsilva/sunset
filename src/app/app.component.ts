e557c07fa4c96e1376f7d649e4b8db2b586a3b99
Alan Silva <alanwvsilva@gmail.com>
2023-12-05 22:36:49 +0000

    Add api integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header.component';
 import { Forecast } from './components/forecast/forecast.component';
@@ -7,7 +8,7 @@ import { Forecast } from './components/forecast/forecast.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Header, Forecast],
+  imports: [CommonModule, HttpClientModule, RouterOutlet, Header, Forecast],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -1,23 +1,27 @@
-import { Component } from '@angular/core';
-import axios from 'axios';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   standalone: true,
   selector: 'forecast',
-  templateUrl: `./forecast.component.html`,
+  templateUrl: './forecast.component.html',
 })
-export class Forecast {
-  async getForecast() {
-    await axios
-      .get('http://api.weatherapi.com/v1/current.json ', {
-        params: {
-          key: '9a2af268c5ae45d59e7170512233007',
-          q: 'London',
-          days: '3',
-        },
-      })
-      .then((response) => {
-        console.log(response.data);
+export class Forecast implements OnInit {
+  data: any = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  private fetchData(): void {
+    this.httpClient
+      .get(
+        `https://api.weatherapi.com/v1/forecast.json?key=9a2af268c5ae45d59e7170512233007&q=toulouse&days=7`
+      )
+      .subscribe((res: any) => {
+        this.data = res;
       });
   }
 }
