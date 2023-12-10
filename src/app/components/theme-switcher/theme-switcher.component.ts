ea85f71442f5037686dc101e8db676eb86d3b40c
Alan Silva <alanwvsilva@gmail.com>
2023-12-10 12:09:02 +0000

    Update theme switcher

--- src/app/components/theme-switcher/theme-switcher.component.ts
+++ src/app/components/theme-switcher/theme-switcher.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'theme-switcher',
   templateUrl: `./theme-switcher.component.html`,
 })
-export class ThemeSwitcher {
-  toggleDarkTheme() {
-    document.documentElement.classList.toggle('dark');
+export class ThemeSwitcher implements OnInit {
+  isDarkTheme: boolean = localStorage.getItem('theme') === 'dark';
+
+  updateTheme(e: Event) {
+    const active = (e.target as HTMLInputElement).checked;
+    console.log(active);
+    this.isDarkTheme = active;
+    localStorage.setItem('theme', active ? 'dark' : 'light');
+    this.setDarkTheme(active);
+  }
+
+  setDarkTheme(active: boolean) {
+    if (active) {
+      document.documentElement.classList.add('dark');
+      document.documentElement.setAttribute('data-theme', 'forest');
+    } else {
+      document.documentElement.classList.remove('dark');
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+  }
+
+  ngOnInit() {
+    this.setDarkTheme(this.isDarkTheme);
   }
 }
